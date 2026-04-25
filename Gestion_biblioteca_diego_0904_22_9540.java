379b1bdbe4c2f4a057232e7e844d2cf667548378
dcastilloc11-lgtm <dcastilloc11@miumg.edu.gt>
2026-04-25 11:26:12 -0600

    Incial "He creado el proyecto y su estructura de paquetes" Agregué La Clase Libro como parte del modelo Agregué la Clase Biblioteca para la parte del Servicio Agregué la Clase main para probar la aplicación

---
+++ Gestion_biblioteca_diego_0904_22_9540.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Diego0904_22_9540.gestionbiblioteca;
+
+/**
+ *
+ * @author umg
+ */
+public class Gestion_biblioteca_diego_0904_22_9540 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
